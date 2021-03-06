f5dd30929b3ef97901c3079d5849fbd45310bd6a
aaron592 <64913505+aaron592@users.noreply.github.com>
2021-03-06 21:17:26 +0530

    Update success.php

--- success.php
+++ success.php
@@ -11,8 +11,8 @@
    $occupation = $_POST['job'];
    $email = $_POST['email'];
    $contact = $_POST['number'];
-   $orig_file = $_FILES["avatar"]["tmp_name"];
-   $ext = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
+   $orig_file = $_FILES["file"]["tmp_name"];
+   $ext = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
    $target_dir = 'uploads/';
    $destination = "$target_dir$contact.$ext";
    move_uploaded_file($orig_file,$destination);
