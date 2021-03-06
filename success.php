e4d2979826cc48dfd8bcc0e140ec4548a8c6191e
aaron592 <64913505+aaron592@users.noreply.github.com>
2021-03-06 19:14:37 +0530

    Update success.php

--- success.php
+++ success.php
@@ -42,24 +42,67 @@
 -->
 
 
-<h2 style="text-align:center">Profile Card</h2>
+<aside class="profile-card">
+  <header>
+    <!-- here’s the avatar -->
+    <a target="_blank" href="#">
+      <img src="<?php echo $destination; ?>" class="hoverZoomLink" style="width: 100%; height: auto" />
+      
+    </a>
 
-<div class="card">
- <img src="<?php echo $destination; ?>" style="width: 30%; height: 30%" />
-  <h1><?php echo $_POST['firstname']. " ". $_POST['lastname']?></h1>
-  <p class="title"><?php echo $_POST['job']?></p>
-  <div style="margin: 24px 0;">
-  <p>Date of birth: <?php echo $_POST['dob']?></p>
-  <p>Email: <?php echo $_POST['email']?></p>
-  <p>Contact no: <?php echo $_POST['number']?></p>
-  
-    <a href="#"><i class="fa fa-dribbble"></i></a> 
-    <a href="#"><i class="fa fa-twitter"></i></a>  
-    <a href="#"><i class="fa fa-linkedin"></i></a>  
-    <a href="#"><i class="fa fa-facebook"></i></a> 
+    <!-- the username -->
+    <h1>
+           <?php echo $_POST['firstname']. " ". $_POST['lastname']?>
+          </h1>
+
+    <!-- and role or location -->
+    <h2>
+            <?php echo $_POST['job']?>
+          </h2>
+
+  </header>
+
+  <!-- bit of a bio; who are you? -->
+  <div class="profile-bio">
+
+    <p>
+     Date of birth: <?php echo $_POST['dob']?>
+    </p>
+    <p>
+      Email: <?php echo $_POST['email']?>
+    </p>
+    
+    <p>
+      Contact no: <?php echo $_POST['number']?>
+    </p>
+
   </div>
-  <p><button>Contact</button></p>
-</div>
+
+  <!-- some social links to show off -->
+  <ul class="profile-social-links">
+    <li>
+      <a target="_blank" href="https://www.facebook.com/creativedonut">
+        <i class="fa fa-facebook"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://twitter.com/dropyourbass">
+        <i class="fa fa-twitter"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://github.com/vipulsaxena">
+        <i class="fa fa-github"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://www.behance.net/vipulsaxena">
+       
+        <i class="fa fa-behance"></i>
+      </a>
+    </li>
+  </ul>
+</aside>
 
 
 
