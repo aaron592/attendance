cb858573956a3b7329646e919f9588e72fcf04e0
aaron592 <64913505+aaron592@users.noreply.github.com>
2021-03-06 20:09:08 +0530

    Update view.php

--- view.php
+++ view.php
@@ -20,29 +20,25 @@
  ?>
 
 
-<img src="<?php echo empty($result['avatar_path']) ? "uploads/blank.png" : $result['avatar_path'] ; ?>" class="rounded-circle" style="width: 20%; height: 20%" />
+<center>
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">
-    <?php echo $result['firstname'] . " ". $result['lastname']; ?>
-    </h6>
-    <h6 class="card-subtitle mb-2 text-muted">
-    <?php echo $result['name']?>
-    </h6>
-    <p class="card-text">
-    Date of birth: <?php echo $result['dateofbirth']?>
-    </p>
-    <p class="card-text">
-    Email: <?php echo $result['emailaddress']?>
-    </p>
-    <p class="card-text">
-    Contact no: <?php echo $result['contactnumber']?>
-    </p>
-    <a href="viewrecord.php" class="btn btn-primary">Back to list</a>
+  <img src="<?php echo empty($result['avatar_path']) ? "uploads/blank.png" : $result['avatar_path'] ; ?>"  style="width: 100%; height: 20%" />
+    <h5 class="card-title"> <?php echo $result['firstname'] . " ". $result['lastname']; ?></h6>
+    <h6 class="card-subtitle mb-2 text-muted"> <?php echo $result['name']?></h6>
+    <p class="card-text">Date of birth: <?php echo $result['dateofbirth']?></p>
+    <p class="card-text">Email: <?php echo $result['emailaddress']?></p>
+    <p class="card-text">Contact no: <?php echo $result['contactnumber']?></p>
+    <div class="card-footer">
+      <small class="text-muted">
+         <a href="viewrecord.php" class="btn btn-primary">Back to list</a>
       <a href="edit.php?id=<?php echo $result['attendee_id'] ?>" class="btn btn-warning">Edit</a>
       <a onclick="return confirm('Are you sure to delete this record?')" href="delete.php?id=<?php echo $result['attendee_id'] ?>" class="btn btn-danger">Delete</a>
+    </small>
+    </div>
   </div>
 </div>
+</center>
 
 
 <?php } ?>
